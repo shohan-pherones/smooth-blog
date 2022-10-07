e918358de0ea076e4fbb2b5732cb08086f35f19b
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-10-08 05:51:22 +0600

    ss added

---
+++ src/script.js
@@ -0,0 +1,18 @@
+import Lenis from "@studio-freight/lenis";
+
+const lenis = new Lenis({
+  duration: 2,
+  easing: (x) => 1 - Math.pow(1 - x, 5),
+  direction: "vertical",
+  gestureDirection: "vertical",
+  smooth: true,
+  smoothTouch: true,
+  touchMultiplier: 2,
+});
+
+const raf = function (time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+};
+
+requestAnimationFrame(raf);
